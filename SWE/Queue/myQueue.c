6f382917996779426b69d7168ffcdf9609e2bc70
Nico <rothenhoefernicolai@gmail.com>
2024-01-14 15:20:01 +0100

    queue functions change

--- SWE/Queue/myQueue.c
+++ SWE/Queue/myQueue.c
@@ -1,10 +1,63 @@
 #include <stdio.h>
 
-#define SIZE 5 // Definieren Sie die Größe der Queue
+#define SIZE 5 
+
+void enqueue(int element);
+int dequeue();
+void show();
+
 
 int queue[SIZE];
 int front = -1;
 int rear = -1;
+
+
+
+int main()
+{
+    int choice;
+    int element = 0;
+
+    while (1)
+    {
+
+        printf("Choose an operation for the queue: \n");
+        printf("enqueue [1]\ndequeue [2]\nfirst [3]\nshow [9]\nexit [0]\n");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+
+            printf("Number to enqueue: ");
+            scanf("%d", &element);
+            enqueue(element);
+            break;
+
+        case 2:
+
+            dequeue();
+            break;
+
+        case 3:
+
+            printf("%d\n", queue[front]);
+            break;
+
+        case 9:
+
+            show();
+            break;
+
+        case 0:
+
+            return 0;
+        }
+    }
+
+    return 0;
+}
+
 
 void enqueue(int element)
 {
@@ -66,49 +119,4 @@ void show()
         }
         printf("%d\n", queue[i]); // print the last element
     }
-}
-
-int main()
-{
-    int choice;
-    int element = 0;
-
-    while (1)
-    {
-
-        printf("Choose an operation for the queue: \n");
-        printf("enqueue [1]\ndequeue [2]\nfirst [3]\nshow [9]\nexit [0]\n");
-        scanf("%d", &choice);
-
-        switch (choice)
-        {
-        case 1:
-
-            printf("Number to enqueue: ");
-            scanf("%d", &element);
-            enqueue(element);
-            break;
-
-        case 2:
-
-            dequeue();
-            break;
-
-        case 3:
-
-            printf("%d\n", queue[front]);
-            break;
-
-        case 9:
-
-            show();
-            break;
-
-        case 0:
-
-            return 0;
-        }
-    }
-
-    return 0;
 }
