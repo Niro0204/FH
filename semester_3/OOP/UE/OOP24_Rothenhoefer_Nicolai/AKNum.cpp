e58722e6dbf85f11ceea4e005aaa1c67942a0eee
Niro0204 <rothenhoefernicolai@gmail.com>
2024-10-03 21:38:08 +0200

    AKNum begin

---
+++ semester_3/OOP/UE/OOP24_Rothenhoefer_Nicolai/AKNum.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class AKNum {
+
+    private:
+        int inputNum; 
+        std::vector<int> digits;
+        int tempResult;
+    
+    public:
+        // Konstruktor
+        AKNum(int input) : inputNum{input}, tempResult{0}{}
+
+        std::vector<int> getDigits(){
+
+            int tempInput = inputNum;
+            int digit{0};   
+
+            if(inputNum < 1000 || inputNum > 9999){
+                std::cerr << "The number has to be 4 digits!" << std::endl;
+            }         
+
+            for(int i = 0; i < 5; i++){
+                digit = tempInput % 10;
+                tempInput = tempInput / 10;
+                digits.push_back(digit);
+            }
+
+            //std::reverse(digits.begin(),digits.end());
+
+            return digits;
+        }
+
+        bool validateInput(){
+
+            int firstDigit = digits[0];
+
+            for(int digit : digits){
+                if(digit != firstDigit){
+                    return true;
+                }
+            }
+        
+
+            std::cerr << "The digits cannot all be the same!" << std::endl;
+            
+            return false;
+        }
+
+        void sortDesc(){
+            sort(digits.begin(), digits.end(), std::greater<int>());
+        }
+
+        void sortAsc(){
+            sort(digits.begin(),digits.end());
+        }
+
+        int vecToNum(){
+
+        }
+
+        int subtract(){
+            
+        }
+
+        void startAlgorithm(){
+
+        }
+
+};
+
+
+int main(){
+
+
+    
+
+
+
+    return 0;
+}
