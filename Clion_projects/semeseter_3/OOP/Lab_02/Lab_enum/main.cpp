22eee79a6407a6b721bca70495f1f826acc5a978
Niro0204 <rothenhoefernicolai@gmail.com>
2024-10-11 13:45:01 +0200

    lab files

--- Clion_projects/semeseter_3/OOP/Lab_02/Lab_enum/main.cpp
+++ Clion_projects/semeseter_3/OOP/Lab_02/Lab_enum/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
+
+
+enum color {
+    red,
+    blue,
+    green
+};
+
+void printColor(color c) {
+
+    switch (c) {
+        case red:
+            std::cout << "Rot";
+        break;
+        case blue:
+            std::cout << "Blau";
+        break;
+        case green:
+            std::cout << "Grün";
+        break;
+    }
+
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    auto c = green;
+    std::cout << c << std::endl;
+
+    printColor(c);
+
+    int ivar = 5;
+
+    // implizite typ umwandlung
+    double dvar = ivar;
+    // explizit im c style --> nix gut, nix machen
+    dvar = (double)ivar;
+    // explizite typumwandlung c++ style
+    dvar = static_cast<double>(ivar); // --> diese gut, static-> zur programmierzeit
+
     return 0;
 }
