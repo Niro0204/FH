8848f449eb05fe5d720032392ead24af5e009b40
Niro0204 <rothenhoefernicolai@gmail.com>
2024-10-12 23:37:12 +0200

    skat files

--- Clion_projects/semeseter_3/OOP/Lab_02/UE/skat/main.cpp
+++ Clion_projects/semeseter_3/OOP/Lab_02/UE/skat/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <array>
+#include <memory>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+enum c {
+    karo,
+    herz,
+    pik,
+    kreuz
+};
+
+enum w {
+    seven,
+    eight,
+    nine,
+    bube,
+    dame,
+    könig,
+    zehn,
+    ass
+};
+
+struct card {
+    c color;
+    w worth;
+};
+
+std::array<std::shared_ptr<card>,32> fillDeck(std::array<std::shared_ptr<card>,32>& cardDeck) {
+
+        int index = 0;
+
+        for(int i = static_cast<int>(c::karo); i <= static_cast<int>(c::kreuz); i++) {
+            for(int j = static_cast<int>(w::seven); j <= static_cast<int>(w::ass); j++) {
+                cardDeck[index]= std::make_shared<card>(static_cast<c>(i), static_cast<w>(j));
+                index++;
+            }
+        }
+
+    return cardDeck;
+}
+
+int main(){
+
+    std::array<std::shared_ptr<card>,32> deck;
+    deck = fillDeck(deck);
+
     return 0;
 }
