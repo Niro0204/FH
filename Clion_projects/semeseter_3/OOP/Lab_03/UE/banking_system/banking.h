4656dcdc0fcc58514258f0f1af1cea5f2e3a4368
Niro0204 <rothenhoefernicolai@gmail.com>
2024-10-22 13:34:34 +0200

    web tech

---
+++ Clion_projects/semeseter_3/OOP/Lab_03/UE/banking_system/banking.h
@@ -0,0 +1,21 @@
+#ifndef BANKING_H
+#define BANKING_H
+
+#include <iostream>
+#include <string>
+
+class Konto {
+    private:
+        std::string name;
+        int betrag;
+
+
+
+
+    public:
+
+    int saveInFile();
+};
+
+
+#endif
