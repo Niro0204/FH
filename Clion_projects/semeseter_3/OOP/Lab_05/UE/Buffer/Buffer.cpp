65b40e9be233ad9306df52f6b52689e4d8492d01
Niro0204 <rothenhoefernicolai@gmail.com>
2024-11-22 17:28:54 +0100

    Buffer project

--- Clion_projects/semeseter_3/OOP/Lab_05/UE/Buffer/Buffer.cpp
+++ Clion_projects/semeseter_3/OOP/Lab_05/UE/Buffer/Buffer.cpp
@@ -1,5 +1,27 @@
 //
 // Created by nico on 22.11.24.
 //
-
+#include <cstring>
 #include "Buffer.h"
+
+#include <iostream>
+#include <ostream>
+
+void Buffer::setData(size_t index, unsigned char data) {
+    if(this->size > index) {
+        this->data[index] = data;
+    }
+    else {
+        std::cerr << "Index out of bounds" << std::endl;
+    }
+}
+
+void Buffer::fill(unsigned char data) {
+    std::memset(this->data,data,this->size);
+}
+
+void Buffer::fillPattern(const unsigned char* pattern, size_t patSize) {
+    for (size_t i = 0; i < this->size; i++) {
+        this->data[i] = pattern[i % patSize];
+    }
+}
--- Clion_projects/semeseter_3/OOP/Lab_05/UE/Buffer/Buffer.h
+++ Clion_projects/semeseter_3/OOP/Lab_05/UE/Buffer/Buffer.h
@@ -5,12 +5,35 @@
 #ifndef BUFFER_H
 #define BUFFER_H
 
-
+#include <cstring>
 
 class Buffer {
 
-};
+private:
+    unsigned int id;
+    unsigned char* data{};
+    size_t size{};
 
+public:
+    Buffer(unsigned int id, unsigned char* data, size_t size){
+        this->id = id;
+        this->data = new unsigned char[size]; // Speicher allokieren
+        std::memcpy(this->data, data, size);  // Daten kopieren
+        this->size = size;
+    }
+
+    Buffer(unsigned int id, size_t size){this->id=id; this->size=size;};
+
+    ~Buffer(){delete[] data;};
+
+    unsigned int getId()const{return this->id;};
+    unsigned char* getData()const{return this->data;};
+    size_t getSize()const{return this->size;};
+
+    void setData(size_t, unsigned char);
+    void fill(unsigned char);
+    void fillPattern(const unsigned char*, size_t);
+};
 
 
 #endif //BUFFER_H
