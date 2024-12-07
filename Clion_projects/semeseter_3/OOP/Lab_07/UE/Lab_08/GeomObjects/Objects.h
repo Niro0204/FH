19d62810fb1e9537ef3af3a1d17b410e888055d3
Nico <rothenhoefernicolai@gmail.com>
2024-12-07 18:23:00 +0100

    UE lab 07 finished

--- Clion_projects/semeseter_3/OOP/Lab_07/UE/Lab_08/GeomObjects/Objects.h
+++ Clion_projects/semeseter_3/OOP/Lab_07/UE/Lab_08/GeomObjects/Objects.h
@@ -9,76 +9,87 @@
 
 class IGeomObj {
 public:
-    virtual void output()=0;
-    virtual double area()=0;
-    virtual double circumference()=0;
+    virtual void output() const =0;
+    virtual double area() const=0;
+    virtual double circumference() const =0;
 
-    virtual ~IGeomObj()=0 {};
+    virtual ~IGeomObj()=0;
 };
 
-//TODO normal class ro interface
-class UniqueID{
+inline IGeomObj::~IGeomObj() {}
+
+class IUniqueID {
+public:
+    virtual unsigned int getID() const = 0;
+    virtual ~IUniqueID() = default;
+};
+
+class UniqueID {
 private:
     static unsigned int id;
-
-public:#
-
-    static unsigned int getNextId(){return id++;};
-
+public:
+    static unsigned int getNextId() { return id++; }
 };
 
-class GeoRect : public IGeomObj {
+class GeoRect : public IGeomObj, public IUniqueID {
 private:
     double a, b;
+    unsigned int id;
+
 public:
-    GeoRect(const double a, const double b){this->a = a; this->b = b;};
+    GeoRect(const double a, const double b) : id(UniqueID::getNextId()) {this->a = a; this->b = b;};
     ~GeoRect() {}
 
 
     virtual double getA() const{return this->a;}
     double getB() const{return this->b;};
+    unsigned int getID() const override{return this->id;};
 
-    virtual void output()const;
-    double area(double a , double b)const;
-    double circumference(double a, double b)const;
+    void output()const override;
+    double area()const override;
+    double circumference()const override;
 
 };
 
-class GeoSquare : public GeoRect {
+class GeoSquare : public GeoRect{
 private:
     double a;
+
 public:
     explicit GeoSquare(const double a) : GeoRect(a,a) { this->a = a; };
     ~GeoSquare() {};
 
-    double getA()const override{return this->a;};
+    double getA()const override {return this->a;};
 
     void output()const override;
-    double area(double a)const;
-    double circumference(double a)const;
+    double area()const override;
+    double circumference()const override;
 
 };
 
-class GeoEllipse : public IGeomObj {
+class GeoEllipse : public IGeomObj, public IUniqueID {
 private:
     double maR,miR;
+    unsigned int id;
 public:
 
-    GeoEllipse(const double maR, const double miR){this->maR = maR; this->miR = miR;};
+    GeoEllipse(const double maR, const double miR) : id(UniqueID::getNextId()) {this->maR = maR; this->miR = miR;};
     ~GeoEllipse() {};
 
     double getmaR() const{return this->maR;};
     double getmiR() const{return this->miR;};
+    virtual unsigned int getID() const{return this->id;};
 
-    virtual void output()const;
-    double area(double maR, double miR)const;
-    double circumference(double maR, double miR)const;
+    void output()const override;
+    double area()const override;
+    double circumference()const override;
 
 };
 
-class GeoCircle : public GeoEllipse {
+class GeoCircle : public GeoEllipse{
 private:
     double r;
+
 public:
     explicit GeoCircle(const double r) : GeoEllipse(r,r) {this->r = r;};
     ~GeoCircle() {};
@@ -86,7 +97,7 @@ public:
     double getR() const{return this->r;};
 
     void output()const override;
-    double area(double r)const;
-    double circumference(double r)const;
+    double area()const override;
+    double circumference()const override;
 };
 #endif //OBJECTS_H
---
+++ Clion_projects/semeseter_3/OOP/Lab_07/UE/Lab_08/GeomObjects/cmake-build-debug/CMakeFiles/GeomObjects.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nico/Dev/FH/Clion_projects/semeseter_3/OOP/Lab_07/UE/Lab_08/GeomObjects/Objects.cpp" "CMakeFiles/GeomObjects.dir/Objects.cpp.o" "gcc" "CMakeFiles/GeomObjects.dir/Objects.cpp.o.d"
+  "/home/nico/Dev/FH/Clion_projects/semeseter_3/OOP/Lab_07/UE/Lab_08/GeomObjects/main.cpp" "CMakeFiles/GeomObjects.dir/main.cpp.o" "gcc" "CMakeFiles/GeomObjects.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
